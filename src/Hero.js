420d1f2cc65262297ab3fc107010afb922a78504
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-19 16:43:07 -0300

    addition: added all the logic to render components and finish the project

--- src/Hero.js
+++ src/Hero.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closeSubmenu } = useGlobalContext();
 
-export default Hero
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Infraestructura de pagos para Internet</h1>
+          <p>
+            Millones de empresas de todos los tamaños, desde startups hasta
+            empresas de la lista Fortune 500, usan el software y las API de
+            Stripe para aceptar pagos, enviar transferencias y gestionar sus
+            negocios en Internet.
+          </p>
+          <button className="btn">Start Now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
